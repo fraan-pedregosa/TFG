289ecaa1a838037a8760752fa99fa4f8b8d8ad16
fraan-pedregosa <franpedregosa@correo.ugr.es>
2024-04-07 18:13:33 +0200

    .

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,15 +1,28 @@
 import os
 
 from flask import Flask
+from pymongo import MongoClient 
+from flask_pymongo import PyMongo
+
+import click
+from flask import current_app, g
+from flask.cli import with_appcontext
+
 
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        SECRET_KEY='7110c8ae51a4b5af97be6534caef90e4bb9bdcb3380af008f90b23a5d1616bf319bc298105da20fe',
+        MONGO_URI='mongodb://localhost:27017/',
+        MONGO_DBNAME='sound',
+        MONGO_COLLECTION='sound_collection',
+        MONGO_COLLECTION_USER='user_collection'
+
     )
+
+    mongo = PyMongo(app)
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
